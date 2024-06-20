1ce38c163a608ef9f0d428feb06d21b6292c736c
Jerome Ghartey <gharteyjerome@gmail.com>
2024-06-20 16:34:54 -0700

    Betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,12 @@
-/*
- *  * File: 4-puts.c
- *   * Author: Your Name
- *    * Description: This program prints a specific string using puts.
- *     */
-
 #include <stdio.h>
 
 /**
- *  * main - Entry point
+ *  * main - prints the phrase "Programming is like building a multilingual puzzle
  *   *
- *    * Return: Always 0 (Success)
+ *    * Return: 0 if exited properly, non-zero otherwise
  *     */
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
 			return (0);
 }
-
