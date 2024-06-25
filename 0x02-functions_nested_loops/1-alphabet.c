830f07d30828d7940d6728afd31f1d77852c4fc2
JK Ghartey <kaphaelghartey@gmail.com>
2024-06-25 05:55:46 -0700

    betty fix

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,12 +5,12 @@
  */
 void print_alphabet(void)
 {
-    char ch;
+	char ch;
 
-    for (ch = 'a'; ch <= 'z'; ch++)
-    {
-        _putchar(ch);
-    }
-    _putchar('\n');
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
 }
 
