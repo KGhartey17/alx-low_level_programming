ba136a938c11dc1efe87842194c2995615e240c2
JK Ghartey <kaphaelghartey@gmail.com>
2024-06-25 06:19:10 -0700

    fix

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,18 +3,18 @@
 /**
  * _islower - checks for lowercase character
  * @c: the character to check
- * 
+ *
  * Return: 1 if c is lowercase, 0 otherwise
  */
 int _islower(int c)
 {
-    if (c >= 'a' && c <= 'z')
-    {
-        return (1);
-    }
-    else
-    {
-        return (0);
-    }
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
 
